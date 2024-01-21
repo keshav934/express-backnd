5aea571c4ecece1496126a3029629dc714c1be7e
Keshav <kkeshavpipar@gmail.com>
2024-01-21 14:12:57 +0530

    added response and error class in utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // require('dotenv').config({ path: "./env" });
 import dotenv from "dotenv"
 import connectDB from "./db/indexdb.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB conection failed!!!", err)
+    })
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+const asyncHandler = (requrestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requrestHandler(req, res, next))
+            .catch((err) => next(err))
+    }
+};
+
+export { asyncHandler };
+
+
+
+
+
+
+
+
+
+
+
+/*  this is another method of using the above method */
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
